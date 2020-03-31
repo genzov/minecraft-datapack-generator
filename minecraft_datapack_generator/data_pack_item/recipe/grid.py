a5c34191108a39e4dd07163925c0c15f47d0f346
Genzo Vandervelden <genzov@gmail.com>
2020-03-31 19:49:32 +0200

    Renamed methods to private

--- minecraft_datapack_generator/data_pack_item/recipe/grid.py
+++ minecraft_datapack_generator/data_pack_item/recipe/grid.py
@@ -21,15 +21,15 @@ class Grid:
         self.slots = [self.tl, self.tm, self.tr, self.ml, self.mm, self.mr, self.bl, self.bm, self.br]
 
     @staticmethod
-    def column_is_empty(rows, index):
+    def _column_is_empty(rows, index):
         return all(row[index] == ' ' for row in rows)
 
     @staticmethod
-    def drop_column(rows, index):
+    def _drop_column(rows, index):
         return [row[:index] + row[index+1:] for row in rows]
 
     @staticmethod
-    def row_is_empty(row):
+    def _row_is_empty(row):
         return all(True if slot == ' ' else False for slot in row)
 
     def get_keys(self) -> Dict[str, Item]:
@@ -55,32 +55,32 @@ class Grid:
 
         # Remove empty columns
         # Start from the last column, otherwise the indexing gets messed up
-        if self.column_is_empty(rows, 2):
-            rows = self.drop_column(rows, 2)
+        if self._column_is_empty(rows, 2):
+            rows = self._drop_column(rows, 2)
 
-        if self.column_is_empty(rows, 0):
-            rows = self.drop_column(rows, 0)
+        if self._column_is_empty(rows, 0):
+            rows = self._drop_column(rows, 0)
 
         # Repeat index 0. If the previous index 0 was empty, it got dropped, in which case this run is the middle row.
         # This is important because otherwise this column might be dropped
         #   when the other two columns might have had a value, dropping the middle one would mess up the pattern.
-        if self.column_is_empty(rows, 0):
-            rows = self.drop_column(rows, 0)
+        if self._column_is_empty(rows, 0):
+            rows = self._drop_column(rows, 0)
 
         top_row = rows[0]
         middle_row = rows[1]
         bottom_row = rows[2]
 
         # Remove empty rows
-        if self.row_is_empty(top_row):
+        if self._row_is_empty(top_row):
             top_row = None
 
-        if self.row_is_empty(bottom_row):
+        if self._row_is_empty(bottom_row):
             bottom_row = None
 
         # Only drop the middle row if one of the previous rows were empty as well.
         # This is important for the same reason as with the columns, it might mess up the pattern otherwise.
-        if self.row_is_empty(middle_row) and (top_row is None or bottom_row is None):
+        if self._row_is_empty(middle_row) and (top_row is None or bottom_row is None):
             middle_row = None
 
         # Join the arrays and filter out empty rows
