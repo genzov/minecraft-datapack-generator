d93f03b4b9bdefb9ac1d97536f5e9e3d35685881
Genzo Vandervelden <genzov@gmail.com>
2020-03-31 17:15:51 +0200

    Add catalog of all items as of 1.15

---
+++ minecraft_datapack_generator/item/__init__.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Item:
+
+    def __init__(self, name: str, namespace: str = 'minecraft'):
+        self.name = name
+        self.namespace = namespace
+
+    def __str__(self):
+        return f'{self.namespace}:{self.name}'
+
+    def __add__(self, other):
+        if isinstance(other, Item):
+            return [self, other]
+        elif isinstance(other, List):
+            return [self] + other
+
+
+    def __mul__(self, other):
+        return [self] * other
